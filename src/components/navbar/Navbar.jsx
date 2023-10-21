3f22676f7f796143b57b01b723f4b5027ca75e3d
GoatX825 <goatxdev7787@gmail.com>
2023-10-21 09:14:46 +0545

    fourth commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 import styles from './navbar.module.css'
 import DarkModeToggle from '../DarkModeToggle/DarkModeToggle';
+import { signOut, useSession } from 'next-auth/react';
 
 const links = [
     {
@@ -39,6 +40,7 @@ const links = [
 
 
 const Navbar = () => {
+  const session = useSession();
   return (
     <div className={styles.container}>
         <Link href="/" className={styles.logo }>GoatX</Link> 
@@ -52,12 +54,15 @@ const Navbar = () => {
 
                 ))
             }
-            <button
-            className={styles.logout}
-            onClick={() => {
-                console.log("Logged out");
-            }}>Logout
-            </button>
+            {
+              session.status === 'unauthenticated' && (
+              <button
+                className={styles.logout}
+                onClick={() => {
+                  signOut();
+                }}>Logout
+              </button>
+            )}
         </div>
     </div>
   )
